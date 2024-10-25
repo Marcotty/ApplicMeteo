fa188713871d80795f032a07a61307c6395d7429
fredm <fredm@MK2>
2024-10-25 16:23:49 +0200

    Add project files.

---
+++ ApplicMeteo/BasicModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Net.Http.Headers;
+using System.Text.Json;
+
+namespace ApplicMeteo
+{
+    public class BasicModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BasicModel(IHttpClientFactory httpClientFactory) =>
+            _httpClientFactory = httpClientFactory;
+
+        public WeatherForecast? WeatherForecasts { get; set; }
+
+        public async Task OnGet()
+        {
+            var httpRequestMessage = new HttpRequestMessage(
+                HttpMethod.Get,
+                "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,precipitation,rain&timezone=Europe%2FLondon&forecast_days=1")
+            {
+                Headers =
+            {
+                /*{ HeaderNames.Accept, "application/vnd.github.v3+json" },
+                { HeaderNames.UserAgent, "HttpRequestsSample" }*/
+            }
+            };
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                using var contentStream =
+                    await httpResponseMessage.Content.ReadAsStreamAsync();
+
+                WeatherForecasts = await JsonSerializer.DeserializeAsync
+                    <WeatherForecast>(contentStream);
+            }
+        }
+    }
+}
---
+++ ApplicMeteo/OpenMeteoService.cs
@@ -0,0 +1,25 @@
+﻿namespace ApplicMeteo
+{
+    public class OpenMeteoService
+    {
+        private readonly HttpClient _httpClient;
+       
+        public OpenMeteoService(HttpClient httpClient)
+        { 
+            _httpClient = httpClient;
+            
+            var stationURL = "https://api.open-meteo.com/v1/forecast";
+
+            _httpClient.BaseAddress = new Uri("https://api.open-meteo.com/v1/forecast");
+        }
+
+       /* public async Task<IEnumerable<GitHubBranch>?> GetAspNetCoreDocsBranchesAsync() =>
+        await _httpClient.GetFromJsonAsync<IEnumerable<GitHubBranch>>(
+            "repos/dotnet/AspNetCore.Docs/branches");
+       */
+       /* public async Task<IEnumerable<WeatherForecast>?> GetWeatherForecastsAsync =>
+        await _httpClient.GetFromJsonAsync<IEnumerable<WeatherForecast>>(
+          "?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,precipitation,rain&timezone=Europe%2FLondon&forecast_days=1");
+        */
+    }
+}
